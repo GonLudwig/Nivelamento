a5d5faf31c0d673dfdcd86288c2cb814e931f7d2
GonLudwig <glaucox.x@gmail.com>
2022-02-08 21:09:27 -0300

    add

--- routes/api.php
+++ routes/api.php
@@ -18,34 +18,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('nivel_ensino', 'App\Http\Controllers\NivelEnsinoController');
+Route::prefix('v1')->middleware('jwt.auth')->group(function(){
+    Route::apiResource('nivel_ensino', 'App\Http\Controllers\NivelEnsinoController');
+    Route::apiResource('nivelamento', 'App\Http\Controllers\NivelamentoController');
+    Route::apiResource('prova', 'App\Http\Controllers\ProvaController');
+    Route::apiResource('componente', 'App\Http\Controllers\ComponenteController');
+    Route::apiResource('questao', 'App\Http\Controllers\QuestaoController');
+    Route::apiResource('alternativa', 'App\Http\Controllers\AlternativaController');
+    Route::apiResource('nivelamentos_provas', 'App\Http\Controllers\NivelamentosProvaController');
+    Route::apiResource('provas_componentes', 'App\Http\Controllers\ProvasComponenteController');
+});
 
-Route::apiResource('nivelamento', 'App\Http\Controllers\NivelamentoController');
-
-Route::apiResource('prova', 'App\Http\Controllers\ProvaController');
-
-Route::apiResource('componente', 'App\Http\Controllers\ComponenteController');
-
-Route::apiResource('questao', 'App\Http\Controllers\QuestaoController');
-
-Route::apiResource('alternativa', 'App\Http\Controllers\AlternativaController');
-
-Route::apiResource('nivelamentos_provas', 'App\Http\Controllers\NivelamentosProvaController');
-
-Route::apiResource('provas_componentes', 'App\Http\Controllers\ProvasComponenteController');
 
 Route::post('login', 'App\Http\Controllers\AuthController@login');
 Route::post('logout', 'App\Http\Controllers\AuthController@logout');
 Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
 Route::post('me', 'App\Http\Controllers\AuthController@me');
-
-// Route::group([
-
-//     'middleware' => 'api',
-//     'namespace' => 'App\Http\Controllers',
-//     'prefix' => 'auth'
-
-// ], function ($router) {
-
-
-// });
